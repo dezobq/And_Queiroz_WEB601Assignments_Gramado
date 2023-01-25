c51215ef9c4eee047ec74430c2a6f0749241928e
dezobq <84927483+dezobq@users.noreply.github.com>
2023-01-24 22:19:54 -0500

    Added cooments to the code
    added some comments to content-card-component.ts and .html

--- And_Queiroz_Gramado/src/app/content-card/content-card.component.ts
+++ And_Queiroz_Gramado/src/app/content-card/content-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; /* WTH not working after all. I have checked that the module is correctly installed and imported in the app.module.ts file, but the error persists. I have also tried restarting the server and clearing the cache, but to no avail. */
 
 import { ContentList } from '../helper-files/content-list';
 import { Content } from '../helper-files/content-interface';
