eec9084b7f5d0b6fb701b3cecc55322884347b7b
dezobq <84927483+dezobq@users.noreply.github.com>
2023-02-09 09:34:30 -0500

    Added two more contents to content-list-component and fixed the interface.ts

--- And_Queiroz_Gramado/src/app/content-list/content-list.component.ts
+++ And_Queiroz_Gramado/src/app/content-list/content-list.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Content } from '../helper-files/content-interface'
+import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
-  styleUrls: ['./content-list.component.scss']
+  styleUrls: ['./content-list.component.scss'],
 })
-
-
 export class ContentListComponent {
   contentArray: Content[] = [
     {
@@ -17,7 +15,7 @@ export class ContentListComponent {
       creator: 'John Doe',
       imgURL: 'https://picsum.photos/id/10/200/300',
       type: 'Type A',
-      tags: ['Tag 1', 'Tag 2', 'Tag 3']
+      tags: ['Tag 1', 'Tag 2', 'Tag 3'],
     },
     {
       id: 2,
@@ -26,7 +24,7 @@ export class ContentListComponent {
       creator: 'Jane Doe',
       imgURL: 'https://picsum.photos/id/11/200/300',
       type: 'Type B',
-      tags: ['Tag 4', 'Tag 5', 'Tag 6']
+      tags: ['Tag 4', 'Tag 5', 'Tag 6'],
     },
     {
       id: 3,
@@ -35,7 +33,7 @@ export class ContentListComponent {
       creator: 'John Smith',
       imgURL: 'https://picsum.photos/id/12/200/300',
       type: 'Type A',
-      tags: ['Tag 7', 'Tag 8', 'Tag 9']
+      tags: ['Tag 7', 'Tag 8', 'Tag 9'],
     },
     {
       id: 4,
@@ -44,7 +42,7 @@ export class ContentListComponent {
       creator: 'Bob',
       imgURL: 'https://picsum.photos/id/13/200/300',
       type: 'Type A',
-      tags: []
+      tags: [],
     },
     {
       id: 5,
@@ -53,7 +51,7 @@ export class ContentListComponent {
       creator: 'Alice',
       imgURL: 'https://picsum.photos/id/14/200/300',
       type: 'Type B',
-      tags: ['Tag 16', 'Tag 17', 'Tag 18']
+      tags: ['Tag 16', 'Tag 17', 'Tag 18'],
     },
     {
       id: 6,
@@ -62,8 +60,36 @@ export class ContentListComponent {
       creator: 'Alice',
       imgURL: 'https://picsum.photos/id/15/200/300',
       type: 'Type A',
-    }
-  ]
+    },
+    //Assignment 3 - Add 3 more contents
+    {
+      id: 7,
+      title: 'Seventh Content',
+      description: 'Description of seventh content',
+      creator: 'Bob',
+      imgURL: 'https://picsum.photos/id/16/200/300',
+      type: '',
+      tags: ['Tag 19', 'Tag 20', 'Tag 21'],
+    },
+    {
+      id: 8,
+      title: 'Eighth Content',
+      description: 'Description of eighth content',
+      creator: 'Bob',
+      imgURL: 'https://picsum.photos/id/17/200/300',
+      type: 'Type A',
+      tags: ['Tag 22', 'Tag 23', 'Tag 24'],
+    },
+    {
+      id: 9,
+      title: 'Ninth Content',
+      description: 'Description of ninth content',
+      creator: 'Bob',
+      imgURL: 'https://picsum.photos/id/17/200/300',
+      //type: 'Type A',
+      tags: ['Tag 22', 'Tag 23', 'Tag 24'],
+    },
+  ];
 
   constructor() {}
 
@@ -73,62 +99,3 @@ export class ContentListComponent {
     console.log(`Content clicked: id: ${content.id}, title: ${content.title}`);
   }
 }
-/* I was trying to do this way, but I was struggling
-
- export class ContentListComponent implements OnInit {
-  @Input() content: Content[];
-  constructor(){
-
-  }
-  ngOnInit() {
-    this.content = {
-      id:0,
-      title: 'First content',
-      creator: 'Creator of the first content',
-      imgURL: 'https://picsum.photos/200',
-      description: 'This is the body of the  first content',
-      type: 'news'
-    },
-    {
-      id:1,
-      title: 'Second content',
-      creator: 'Creator of the second content',
-      imgURL: 'https://picsum.photos/200',
-      description: 'This is the body of the second content',
-      type: 'news'
-    },
-    {
-      id:2,
-      title: 'Thrird content',
-      creator: 'Creator of the third content',
-      imgURL: 'https://picsum.photos/200',
-      description: 'This is the body of the third content',
-      type: 'news'
-    },
-    {
-      id:3,
-      title: 'Fourth content',
-      creator: 'Creator of the fourth content',
-      imgURL: 'https://picsum.photos/200',
-      description: 'This is the body of the fourth content',
-      type: 'news'
-    },
-    {
-      id:4,
-      title: 'Fifth content',
-      creator: 'Creator of the fifth content',
-      imgURL: 'https://picsum.photos/200',
-      description: 'This is the body of the fifth content',
-      type: 'news'
-    },
-    {
-      id:5,
-      title: 'Sixth content',
-      creator: 'Creator of the sixth content',
-      imgURL: 'https://picsum.photos/200',
-      description: 'This is the body of the sixth content',
-      type: 'news'
-    }
-  }
-} */
-
